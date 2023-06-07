417d12689d96692184d3651dd05272e9ddcd0deb
kelvin-vd-gaag <39948485+kelvin-vd-gaag@users.noreply.github.com>
2023-06-07 13:06:32 +0200

    finished demonstration

--- index.php
+++ index.php
@@ -3,29 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<nav>
-    <img src="cloud-9-logo.png" alt="">
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">E-sports</a></li>
-        <li><a href="#">Spelers</a></li>
-        <li><a href="#">Wedstrijden</a></li>
-    </ul>
-</nav>
+<?php require_once "nav.php" ?>
 <header>
     <div class="brand-name">
         Cloud9
     </div>
-    <div class="header-title">  
+    <div class="header-title">
         Core Collection
     </div>
 </header>
 <div class="intro-text">
-    <p>Since 2013, Cloud9 has become one of the most recognizable esports organizations in the world. With championships industry-wide, unmatched viewership hours, and extensive benefits packages for players and staff, Cloud9 prides itself on being the best in all categories.</p>
+    <h2>What is cloud9</h2>
+    <p>Since 2013, Cloud9 has become one of the most recognizable esports organizations in the world. With championships
+        industry-wide, unmatched viewership hours, and extensive benefits packages for players and staff, Cloud9 prides
+        itself on being the best in all categories.</p>
 </div>
-<img src="banner-player.jpg" alt="Our top fragger playing CS GO" width="600" height="200">
-
+]
+<div class="banner">
+    <img src="banner-player.jpg" alt="Our top fragger playing CS GO" width="1000" height="300">
+</div>
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <img src="cloud-9-logo.png" alt="">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">E-sports</a></li>
+        <li><a href="#">Spelers</a></li>
+        <li><a href="wedstrijden.php">Wedstrijden</a></li>
+        <li><a href="about-us.php">Over ons team</a></li>
+    </ul>
+</nav>
