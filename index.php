c5610a3f87285cafc2126e008283c484c68daf1f
kelvin-vd-gaag <39948485+kelvin-vd-gaag@users.noreply.github.com>
2023-06-07 12:33:37 +0200

    opzet project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">E-sports</a></li>
+        <li><a href="#">Spelers</a></li>
+        <li><a href="#">Wedstrijden</a></li>
+    </ul>
+</nav>
+<header>
+    <div class="brand-name">
+        Cloud9
+    </div>
+    <div class="header-title">  
+        Core Collection
+    </div>
+</header>
+<div class="intro-text">
+    <p>Since 2013, Cloud9 has become one of the most recognizable esports organizations in the world. With championships industry-wide, unmatched viewership hours, and extensive benefits packages for players and staff, Cloud9 prides itself on being the best in all categories.</p>
+</div>
+<img src="" alt="">
+
+</body>
+</html>
