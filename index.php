1ade9c927810de0f007c4ba43e363781aaa19b9d
kelvin-vd-gaag <39948485+kelvin-vd-gaag@users.noreply.github.com>
2023-06-07 12:36:33 +0200

    HTML finished

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
 <nav>
+    <img src="cloud-9-logo.png" alt="">
     <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">E-sports</a></li>
@@ -24,7 +25,7 @@
 <div class="intro-text">
     <p>Since 2013, Cloud9 has become one of the most recognizable esports organizations in the world. With championships industry-wide, unmatched viewership hours, and extensive benefits packages for players and staff, Cloud9 prides itself on being the best in all categories.</p>
 </div>
-<img src="" alt="">
+<img src="banner-player.jpg" alt="Our top fragger playing CS GO" width="600" height="200">
 
 </body>
 </html>
